97875f10845b981ed09855c514ac24e6421dcf55
Bamdad <aghilide@ualberta.ca>
2020-05-21 18:49:56 -0400

    instead of jsx, you can use React.createElement()

--- React/react-basics/src/App.js
+++ React/react-basics/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>learn react</h1>
-  );
+class App extends Component {
+  render(){
+    return React.createElement('div',{className: 'App',id: '1'},React.createElement('p',null,"hoooy"));
+    return (
+      <div class="hiiie">
+        <h1>Hiii</h1>
+        <h2> I'm here</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
