fc2cbbb2acbbb5f1cb9f1ff40be5d07e55ea33b1
Bamdad <aghilide@ualberta.ca>
2020-05-21 23:56:17 -0400

    examples of using state and props

--- React/react-basics/src/App.js
+++ React/react-basics/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render(){
-    return React.createElement('div',{className: 'App',id: '1'},React.createElement('p',null,"hoooy"));
+  state = {
+    persons: [
+      {name: "Max", age: 22},
+      {name: "Emily", age: 23}
+    ],
+  }
+  switchNameHandler = () =>{
+      console.log('clicked');
+      this.setState({persons: [{name: "Max1", age: 22},
+      {name: "Emily1", age: 23}]})
+  }
+  render() {
     return (
-      <div class="hiiie">
-        <h1>Hiii</h1>
-        <h2> I'm here</h2>
+      <div className="hiiie">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>ffff</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>ffff</Person>
       </div>
     );
   }
