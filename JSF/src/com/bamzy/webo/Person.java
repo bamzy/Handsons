73496b9aba72656160826ae368c914c745f61918
Bamdad <aghilide@ualberta.ca>
2020-05-24 15:37:19 -0400

    an example of successfully using @ManagedBean

--- JSF/src/com/bamzy/webo/Person.java
+++ JSF/src/com/bamzy/webo/Person.java
@@ -2,15 +2,35 @@ package com.bamzy.webo;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
 
-@ManagedBean (name = "student")
+@ManagedBean (name = "person")
 @RequestScoped
 public class Person {
     String firstName;
     String lastName;
+    String[] manyCheckbox;
+    ArrayList<String> countryList;
+    String favoriteLanguage;
+
+    int countryCode;
 
     public Person() {
+        countryList = new ArrayList<>();
+        countryList.add("IRAN");
+        countryList.add("CANADA");
+        countryList.add("USA");
+        countryList.add("Kuwait");
+        countryList.add("UAE");
+        countryList.add("GERMANY");
+
     }
+
+    public ArrayList<String> getCountryList() {
+        return countryList;
+    }
+
+
 
     public String getFirstName() {
         return firstName;
@@ -26,5 +46,28 @@ public class Person {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getCountryCode() {
+        return this.countryCode;
+    }
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String[] getManyCheckbox() {
+        return this.manyCheckbox;
+    }
+
+    public void setManyCheckbox(String manyCheckbox[]) {
+        this.manyCheckbox = manyCheckbox;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
