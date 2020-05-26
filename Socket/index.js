4eb6d773b4888eadccbe72b923888d8956dcdf7d
Bamdad <aghilide@ualberta.ca>
2020-05-26 17:27:10 -0400

    So far was done using the sample

--- Socket/index.js
+++ Socket/index.js
@@ -1,8 +1,12 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
-io.on("connection", function(socket) {
-    console.log("socket connected");
-   });
+app.get("/", (req, res) =>
+    res.sendFile(__dirname + "/index.html"));
+app.
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+    });
 http.listen(3000, () => console.log("listening on http://localhost:3000"));
