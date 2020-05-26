7d9df3bc9258187462b31b2df1d223e89a430afe
Bamdad <aghilide@ualberta.ca>
2020-05-26 19:28:24 -0400

    started the Udemy tutorial

--- Socket/index.js
+++ Socket/index.js
@@ -5,15 +5,15 @@ app.get("/", (req, res) =>
     res.sendFile(__dirname + "/index.html"));
 
 io.on('connection', (socket) => {
-    io.emit('broadcast','welcom to Our Group');
+    io.emit('broadcast','Welcom to Our Group');
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
       });
     socket.on('typing status', (msg) => {
         if (msg)
-            io.emit('typing status', "Someone is typing");
+            socket.broadcast.emit('typing status', "Someone is typing");
             else
-            io.emit('typing status', "...");
+            socket.broadcast.emit('typing status', "...");
     });
 });
     
