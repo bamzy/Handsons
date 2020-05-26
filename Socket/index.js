05b507f75cc2f6e538aab76dbd89858f9b16263d
Bamdad <aghilide@ualberta.ca>
2020-05-26 17:45:42 -0400

    a basic typing in progress detector

--- Socket/index.js
+++ Socket/index.js
@@ -3,10 +3,17 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 app.get("/", (req, res) =>
     res.sendFile(__dirname + "/index.html"));
-app.
+
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
       });
+    socket.on('typing status', (msg) => {
+        if (msg)
+            io.emit('typing status', "Someone is typing");
+            else
+            io.emit('typing status', "...");
     });
+});
+    
 http.listen(3000, () => console.log("listening on http://localhost:3000"));
