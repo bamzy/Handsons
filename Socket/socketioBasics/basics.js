ac7f2a2688ee3f19233a2f6044208fec431bbb8e
Bamdad <aghilide@ualberta.ca>
2020-05-27 14:50:09 -0400

    starting 3rd day of socket.io training

--- Socket/socketioBasics/basics.js
+++ Socket/socketioBasics/basics.js
@@ -1,4 +1,19 @@
-import http from 'http';
+// We nned http because we don't have express
+const http = require('http');
+
+//3rd party library install with npm earlier
+const socketio = require('socket.io')
+
+//We make an http server with node
 const server = http.createServer((request,result) =>{
-    
+    result.end("Bye Felicia");
 })
+
+const io = socketio(server);
+io.on('connection', (socket,req) => {
+    socket.emit('welcome','Welcome to the websocket server!!');
+    socket.on('my other event', (msg)=> {
+        console.log(msg);
+    })
+});
+server.listen(8000);
