28ca3687260ea081142b802e89dab29742d56aea
Bamdad <aghilide@ualberta.ca>
2020-05-28 19:41:08 -0400

    prettified

--- Socket/slackApp/public/scripts.js
+++ Socket/slackApp/public/scripts.js
@@ -2,28 +2,28 @@ $(function () {
     var socket = io('http://localhost:8000'); //the / is a namespace
     var socketAdmin = io('http://localhost:8000/admin'); //the /admin is a namespace
     var socketMarketing = io('http://localhost:8000/marketing'); //the /marketing is a namespace
-    $('form').submit(function(e){
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      $('#infoMessage').text('...');
-      return false;
+    $('form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        $('#infoMessage').text('...');
+        return false;
     });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg.sender +":"+msg.message));
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg.sender + ":" + msg.message));
     });
-    socket.on('typing status', function(msg){
-      $('#infoMessage').text(msg);
-  });
-  
-  $('#m').keydown(function (e){
-      socket.emit("typing status",{status: true, id: socket.id} )
-  });
-  $('#m').keyup(function (e){
-      socket.emit("typing status",{status: false, id: socket.id})
-  });
-  socket.on('broadcastFromServer', function(msg){
-      $('#infoMessage').text(msg);
+    socket.on('typing status', function (msg) {
+        $('#infoMessage').text(msg);
+    });
+
+    $('#m').keydown(function (e) {
+        socket.emit("typing status", { status: true, id: socket.id })
+    });
+    $('#m').keyup(function (e) {
+        socket.emit("typing status", { status: false, id: socket.id })
+    });
+    socket.on('broadcastFromServer', function (msg) {
+        $('#infoMessage').text(msg);
 
     });
-  });
+});
