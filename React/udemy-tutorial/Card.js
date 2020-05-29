091357911fe5b6693014bfe8e03ab05aa000064a
Bamdad <aghilide@ualberta.ca>
2020-05-29 18:59:06 -0400

    got the issue fixed

--- React/udemy-tutorial/Card.js
+++ React/udemy-tutorial/Card.js
@@ -1,21 +1,20 @@
-import React from 'react' ;
-class Card extends Component{
+
+class Card extends React.Component{
     render(){
-    return (
-            <div className="col s2">
-                <div className="card hoverable small">
-                    <div className="card-image">
-                        <img src="http://lorempixel.com/600/600" />
-                    </div>
-                    <div className="card-content">
-                        <p>Title</p>
-                        <p>This is the second</p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#">CAD 10.00</a>
-                    </div>
-                </div>
-            </div>);
+    return <div className="col s2">
+                 <div className="card hoverable small">
+                     <div className="card-image">
+                         <img src={this.props.data.pixel} />
+                     </div>
+                     <div className="card-content">
+                         <p>{this.props.data.name}</p>
+                         <p>{this.props.data.description}</p>
+                     </div>
+                     <div className="card-action">
+                         <a href="#">CAD {this.props.data.price}</a>
+                     </div>
+                 </div>
+             </div>;
     }
     
 }
---
+++ React/udemy-tutorial/data.js
@@ -0,0 +1,74 @@
+const data = [
+      {
+            id: 1,
+            name: 'aaaa',
+            description: 'aaaa',
+            pixel: "http://lorempixel.com/600/601"
+      },
+      {
+            id: 2,
+            name: 'bbbb',
+            description: 'bbbbbbb',
+            pixel: "http://lorempixel.com/600/602"
+      },
+      {
+            id: 3,
+            name: 'cccc',
+            description: 'cccccc',
+            pixel: "http://lorempixel.com/600/603"
+      },
+      {
+            id: 4,
+            name: 'dddd',
+            description: 'ddd',
+            pixel: "http://lorempixel.com/600/604"
+      },
+      {
+            id: 5,
+            name: 'eeee',
+            description: 'eeeeee',
+            pixel: "http://lorempixel.com/600/605"
+      },
+      {
+            id: 7,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      },
+      {
+            id: 6,
+            name: 'ffff',
+            description: 'fffff',
+            pixel: "http://lorempixel.com/600/606"
+      }
+]
