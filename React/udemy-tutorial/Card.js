303c728890d0adf5cbb6cd366f3ee3e511e935fa
Bamdad <aghilide@ualberta.ca>
2020-05-29 20:58:33 -0400

    an example of events and states

--- React/udemy-tutorial/Card.js
+++ React/udemy-tutorial/Card.js
@@ -1,5 +1,17 @@
 
 class Card extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            name : 'Sold Out'
+        };
+        setInterval(()=>{
+            this.setState({name: 'Available'})
+        },5000);
+    }
+    handleReserve= ()=>{
+        this.setState({name: "Reserved"});
+    }
     render(){
     return <div className="col s2">
                  <div className="card hoverable small">
@@ -8,10 +20,11 @@ class Card extends React.Component{
                      </div>
                      <div className="card-content">
                          <p>{this.props.data.name}</p>
-                         <p>{this.props.data.description}</p>
+                         <p>{this.state.name}</p>
                      </div>
                      <div className="card-action">
                          <a href="#">CAD {this.props.data.price}</a>
+                         <button id="resBtn" className='btn' onClick={this.handleReserve}>Rserve</button>
                      </div>
                  </div>
              </div>;
--- React/udemy-tutorial/data.js
+++ React/udemy-tutorial/data.js
@@ -3,67 +3,78 @@ const data = [
             id: 1,
             name: 'aaaa',
             description: 'aaaa',
-            pixel: "http://lorempixel.com/600/601"
+            pixel: "http://lorempixel.com/600/601",
+            price: 12.00
       },
       {
             id: 2,
             name: 'bbbb',
             description: 'bbbbbbb',
-            pixel: "http://lorempixel.com/600/602"
+            pixel: "http://lorempixel.com/600/602",
+            price: 12.00
       },
       {
             id: 3,
             name: 'cccc',
             description: 'cccccc',
-            pixel: "http://lorempixel.com/600/603"
+            pixel: "http://lorempixel.com/600/603",
+            price: 12.00
       },
       {
             id: 4,
             name: 'dddd',
             description: 'ddd',
-            pixel: "http://lorempixel.com/600/604"
+            pixel: "http://lorempixel.com/600/604",
+            price: 12.00
       },
       {
             id: 5,
             name: 'eeee',
             description: 'eeeeee',
-            pixel: "http://lorempixel.com/600/605"
+            pixel: "http://lorempixel.com/600/605",
+            price: 12.00
       },
       {
             id: 7,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
             name: 'ffff',
             description: 'fffff',
-            pixel: "http://lorempixel.com/600/606"
+            pixel: "http://lorempixel.com/600/606",
+            price: 12.00
       },
       {
             id: 6,
