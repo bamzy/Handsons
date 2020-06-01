e089cde6efcb85f1635040e384632d7d0224912e
Bamdad <aghilide@ualberta.ca>
2020-06-01 15:05:07 -0400

    added a search box

--- React/create-react-app/src/WeatherCard.js
+++ React/create-react-app/src/WeatherCard.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 class WeatherCard extends Component{
-    constructor(){
-        super();
-    }
+    
 
     componentDidMount(){
         var elems = document.querySelectorAll('.modal');
@@ -10,17 +8,27 @@ class WeatherCard extends Component{
     }
     render(){
         const icon = (typeof this.props.info.weather === 'undefined')? "":this.props.info.weather[0].icon;
-        console.log(icon);
         const img = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
         return (
-            <div className="row">
-                <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>          
+            <div className="container">
+                <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Details</a>          
                 <div id="modal1" className="modal">
-                <div className="modal-content">
-        <h4>{this.props.info.name}</h4>
-                    <p><img src={img} /><b>Feels Like: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.feels_like}</p>
-                    <p><b>Temp: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.temp}</p>
-                    
+                    <div className='container'>
+                    <h4>{this.props.info.name}</h4>
+                    </div>
+                        <div className="modal-content">
+                            <div className='row'>
+                                <div className="col s3">
+                                <img src={img} />
+                            </div>
+                            <div class="col s3">
+                                <p><b>Feels Like: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.feels_like} C</p>
+                                <p><b>Temp: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.temp}</p>
+                                <p><b>Min Temp: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.temp_min}</p>
+                                <p><b>Max Temp: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.temp_max}</p>
+                                <p><b>Humidity: </b>{(typeof this.props.info.main === 'undefined')? "":this.props.info.main.humidity}</p>
+                            </div>
+                    </div>
                 </div>
                 <div className="modal-footer">
                     <a href="#!" className="modal-close waves-effect waves-green btn-flat">Got It</a>
--- React/create-react-app/src/WeatherCardSet.js
+++ React/create-react-app/src/WeatherCardSet.js
@@ -6,21 +6,38 @@ class WeatherCardSet extends Component{
       constructor(){
             super();
             this.state = {
-                  info: ""
+                  info: "",
+                  name: ""
             }
       }
       componentDidMount(){
-            const url = 'https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=e312dbeb8840e51f92334498a261ca1d';
+            const url = 'https://api.openweathermap.org/data/2.5/weather?q='+document.querySelector('#inputCityName').value+'&units=metric&appid=e312dbeb8840e51f92334498a261ca1d';
             axios.get(url).then((resp)=>{
               this.setState({
-                  info: resp.data
+                  info: resp.data,
+                  name: resp.data.name
               });
             });
           }
+      searchCity = (event)=>{
+            event.preventDefault();
+            const cityName = document.querySelector('#inputCityName').value;
+            const url = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName +'&units=metric&appid=e312dbeb8840e51f92334498a261ca1d';
+            axios.get(url).then((resp)=>{
+              this.setState({
+                  info: resp.data,
+                  name: resp.data.name
+              });
+            });
+      }
      
       render(){
             
-            return (<div className="col s2" >                        
+            return (<div className="container" >
+                  <h1>{this.state.name}</h1>
+                  <form onSubmit={this.searchCity}>
+                        <input type="text" id='inputCityName' placeholder="Enter city name"></input>
+                  </form>                        
                         <WeatherCard info={this.state.info}  />
                   </div>
                   );
