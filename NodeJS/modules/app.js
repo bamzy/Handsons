a18ab11fd08c48efa99c2bd46b0b700294f37f84
Bamdad <aghilide@ualberta.ca>
2020-06-14 19:56:53 -0400

    in case someone needs to create a server in such a low level

--- NodeJS/modules/app.js
+++ NodeJS/modules/app.js
@@ -6,3 +6,24 @@ logger.on('messageLogged',(args)=>{
     console.log(args);
 })
 logger.log('hello world')
+
+
+const http = require('http');
+const server = http.createServer((req,resp)=>{
+    if (req.url === '/'){
+        resp.write('hello client');
+    }
+    if (req.url === '/client/api/v1'){
+        resp.write(JSON.stringify([1,2,3,4,5]));
+        
+    }
+    resp.end();
+});
+server.on('connection',(socket)=>{
+    console.log('Client Detected');
+})
+
+
+
+
+server.listen(3000);
