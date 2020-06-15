b5b9652ee36c05dbcc185a7725de4e9f94231ae8
Bamdad <aghilide@ualberta.ca>
2020-06-15 16:34:57 -0400

    this is a simple mongoos usage in logging

--- NodeJS/modules/logs/logger.js
+++ NodeJS/modules/logs/logger.js
@@ -1,13 +1,36 @@
 const EventEmitter = require('events');
+const chalk = require('chalk');
+const Moment = require('moment');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+
+
+
 
 
 url = 'http://www.google.com/log';
 
 class Logger extends EventEmitter {
+    constructor(){
+        super();
+        mongoose.connect('mongodb://localhost:27017/flights', {useNewUrlParser: true, useUnifiedTopology: true});
+        
+        this.Log = mongoose.model('Log', { msg: String });
+        this.moment = Moment();
+    }
     log(message){
-        console.log(`message logged: ${message}`);
-        this.emit('messageLogged',{id:3});
-    }    
+        console.log(chalk.blueBright(`message logged => ${message}`));
+        const log = new this.Log({ msg: message });
+        log.save();
+        this.logFile(message);
+
+    }
+    logFile(message){
+        fs.appendFile('logs.txt', `file logged at ${this.moment.format('YYYY-MM-DD hh:mm:ss')} => ${message} \n`,(err)=>{
+            if (err) throw err;
+        })
+    }
 }
 module.exports = Logger;
 
