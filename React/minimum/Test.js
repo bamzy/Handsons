3889f668911f3d5e05b27c91d44a78a847eca373
Bamdad <bamdad@mortgageautomator.com>
2022-11-29 17:58:25 -0500

    finally a bare minimum

---
+++ React/minimum/Test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
+class LoveButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You loved this.';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) , className:"hi"},
+            e('img',{src:'https://codecademy-prod.s3.amazonaws.com/profile_thumbnail/598aa9ff961baeacab0002ef_560996145.png',style:{width:40 +'px',height:40+'px'}},null)
+        );
+    }
+}
