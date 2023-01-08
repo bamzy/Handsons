d4a589d619591ea18780aad927cca109899f4517
Bamdad <bamdad@mortgageautomator.com>
2023-01-08 18:03:42 -0500

    passing between middlewares using request just like how passport is doing it

--- NodeJS/express-passport/app.js
+++ NodeJS/express-passport/app.js
@@ -3,19 +3,31 @@ const app = new express();
 
 const firstMW = function(req,res,next){
     // res.send('<h2>first</h2>');
-    console.log('first middleware');
+    console.dir('first middleware, req:');
+    req.customField = 'bamdad';
+
     next();
 }
-const secondMW = function(req,res,next){
+const secondMW = (req,res,next) => {
     // res.send('<h2>first</h2>');
-    console.log('second middleware');
+    console.log('second middleware, req:'+req);
+    req.customField += ' Kordi' ;
     next();
 }
+const errHandlerMW = (err, req,res,next) =>{
+    if(err) {
+        res.send('This error happened'+err);
+        console.log("This error happened:" + err);
+    }
+}
+app.use(firstMW); //global middleware
 app.use(secondMW); //global middleware
-app.get('/',firstMW,(req,res)=>{
+app.get('/',(req,res)=>{ //route specific middleware
     console.log('main middleware');
+    console.log(req.customField);
     res.send('<h1>sanity check</h1>');
 })
-app.listen(3000,()=>{
+app.listen(3001,()=>{
     console.log('app is running');
 })
+app.use(errHandlerMW); //global middleware for error handling comes last
