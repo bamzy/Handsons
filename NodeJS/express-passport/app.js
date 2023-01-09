38d55a72113c33fb1798a73ad77cffac6d6dbb3f
Bamdad <bamdad@mortgageautomator.com>
2023-01-08 20:06:29 -0500

    a working session with Mongo + data retrieval

--- NodeJS/express-passport/app.js
+++ NodeJS/express-passport/app.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+
+const MongoStore = require('connect-mongo');
+const {Schema} = require("mongoose");
+
+
+const dbUri = "mongodb+srv://root:salam@bamdad-cluster.xjbfs7r.mongodb.net/?retryWrites=true&w=majority";
+
 const app = new express();
 
+
+const mongoSessionStore =  MongoStore.create({
+     mongoUrl: dbUri
+});
+
+mongoose.connect(dbUri);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", async function () {
+    console.log("DB Connected successfully");
+    const User = mongoose.model('bamdads', Schema({
+
+        name: String
+    }));
+
+
+
+    const all = await User.find();
+    console.log(all);
+});
 const firstMW = function(req,res,next){
     // res.send('<h2>first</h2>');
     console.dir('first middleware, req:');
@@ -20,8 +49,23 @@ const errHandlerMW = (err, req,res,next) =>{
         console.log("This error happened:" + err);
     }
 }
+
+
 app.use(firstMW); //global middleware
 app.use(secondMW); //global middleware
+app.use(express.urlencoded({extended:true})) //this is very convenient
+app.use(express.json()) //this is very convenient
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 5*1000 //in milisecond
+    },
+    store: mongoSessionStore
+}))
 app.get('/',(req,res)=>{ //route specific middleware
     console.log('main middleware');
     console.log(req.customField);
